5c8beb21b768d11109800bba2f41264e390ea233
Jayed <jayed.swe@gmail.com>
2026-02-01 16:38:36 +0600

    change some code and styles

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,34 +1,37 @@
-import {View, StyleSheet, Text} from 'react-native'
+import React from 'react';
+import {View, StyleSheet, Text, Platform} from 'react-native';
 
 const Home = () => {
-    return (
-        <View style={styles.homeContainer}>
-            <View style={styles.contact}>
-                <Text style={styles.homeTitle}>Hello, Home </Text>
-            </View>
-        </View>
-    )
+  return (
+    <View style={styles.homeContainer}>
+      <View style={styles.home}>
+        <Text style={styles.homeTitle}>Hello, Home</Text>
+      </View>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    homeContainer: {
-        flex: 1,
-    },
-    home: {
-        flex: 1,
-        padding: 5,
-        ...Platform.select({
-            ios: {
-                marginBottom: "0%"
-            },
-            android: {
-                marginBottom: 0
-            }
-        })
-    },
-    homeTitle: {
-        textAlign: "center"
-    }
+  homeContainer: {
+    flex: 1,
+  },
+  home: {
+    flex: 1,
+    padding: 5,
+    justifyContent: 'center', // vertical center
+    alignItems: 'center', // horizontal center
+    ...Platform.select({
+      ios: {
+        marginBottom: '0%',
+      },
+      android: {
+        marginBottom: 0,
+      },
+    }),
+  },
+  homeTitle: {
+    textAlign: 'center',
+  },
 });
 
 export default Home;
